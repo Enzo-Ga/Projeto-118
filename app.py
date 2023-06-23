d98f0e3d4b2c72f05354a7cca89db229c650e0b1
Enzo-Ga <93016718+Enzo-Ga@users.noreply.github.com>
2023-06-22 23:49:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, jsonify
+import prediction
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# API para receber solicitações POST e prever sentimentos
+@app.route('/predict', methods=['POST'])
+def predict():
+    response = {}
+    review = request.json.get('customer_review')
+    
+    if not review:
+        response = {'status': 'error',
+                    'message': 'Avaliação em Branco'}
+    else:
+        sentiment, path = prediction.predict(review)
+        response = {'status': 'success',
+                    'message': 'Got it',
+                    'sentiment': sentiment,
+                    'path': path}
+
+    return jsonify(response)
+
+
+# API para salvar a avaliação quando o usuário clica no botão Salvar
+@app.route('/save-entry', methods=['POST'])
+def save():
+    date = request.json.get('date')
+    product = request.json.get('product')
+    review = request.json.get('review')
+    sentiment = request.json.get('sentiment')
+
+    data_entry = f"{date},{product},{review},{sentiment}\n"
+
+    with open("./static/assets/datafiles/data_file.csv", "a") as f:
+        f.write(data_entry)
+
+    return jsonify({'status': 'success',
+                    'message': 'Dados Registrados'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+from flask import Flask, render_template, request, jsonify
+import prediction
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# API para receber solicitações POST e prever sentimentos
+@app.route('/predict', methods=['POST'])
+def predict():
+    response = {}
+    review = request.json.get('customer_review')
+    
+    if not review:
+        response = {'status': 'error',
+                    'message': 'Avaliação em Branco'}
+    else:
+        sentiment, path = prediction.predict(review)
+        response = {'status': 'success',
+                    'message': 'Got it',
+                    'sentiment': sentiment,
+                    'path': path}
+
+    return jsonify(response)
+
+
+# API para salvar a avaliação quando o usuário clica no botão Salvar
+@app.route('/save-entry', methods=['POST'])
+def save():
+    date = request.json.get('date')
+    product = request.json.get('product')
+    review = request.json.get('review')
+    sentiment = request.json.get('sentiment')
+
+    data_entry = f"{date},{product},{review},{sentiment}\n"
+
+    with open("./static/assets/datafiles/data_file.csv", "a") as f:
+        f.write(data_entry)
+
+    return jsonify({'status': 'success',
+                    'message': 'Dados Registrados'})
+
+if __name__ == "__main__":
+    app.run(debug=True)
